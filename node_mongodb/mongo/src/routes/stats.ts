baa1c21cd47914df3f41422c1ba5a4127ff7e463
Hamza Nadeem <hamzadev866@gmail.com>
2025-09-29 16:23:43 +0500

    Initial commit: Add Node.js MongoDB and MySQL listing assessment projects

--- node_mongodb/mongo/src/routes/stats.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import StatsController from '../controllers/StatsController.js';
-
-const router = express.Router();
-
-router.get('/active-agents', StatsController.getActiveAgents);
-
-export default router;
